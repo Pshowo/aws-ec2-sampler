bde7f1651123e6852144b0f69d88b15171c67d2a
Pawel <pjuszcze@gmail.com>
2023-02-06 18:34:24 +0100

    Added function to get basic value

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
+import psutil
+import os
+
+
+def measure():
+    stats = {
+        "process": os.getpid(),
+        "cpu_per": int(psutil.cpu_percent()),
+        "ram_per": psutil.virtual_memory().percent,
+        "ram_total_GB": round(psutil.virtual_memory().total / 1073741824, 2),
+    }
+    return stats
+
+
 if __name__ == "__main__":
-    pass
+    print(measure())
